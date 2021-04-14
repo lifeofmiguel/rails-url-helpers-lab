7e93b5d684714452c0edab6d5033a71c67c7cbae
Miguel Angel Morales <mmorales21500@live.com>
2021-04-13 23:18:22 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,6 +17,7 @@ ActiveRecord::Schema.define(version: 1) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "active",     default: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Schema.define(version: 1) do
+
+    create_table "students", force: :cascade do |t|
+      t.string   "first_name"
+      t.string   "last_name"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.boolean  "active",     default: false
+    end
+  
+  end
